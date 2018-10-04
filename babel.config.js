3d8c4192375f45e163335c4e56ae167f899fb026
Kav Singh <flakisms@gmail.com>
2018-10-04 10:48:58 +0200

    tests working

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: [
+    // modules, according to docs, defaults to commonjs
+    // leaving this option out, however, causes tests to fail. maybe docs wrong
+    ['@babel/preset-env', { modules: 'commonjs' }],
+    '@babel/preset-typescript',
+  ],
   plugins: ['@babel/plugin-proposal-class-properties'],
 };
