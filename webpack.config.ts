620f21590ddc01b2101ff251ae56a18ffa870712
Kav Singh <flakisms@gmail.com>
2019-07-02 23:27:32 +0200

    remove superfluous typings for webapck config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,11 @@
 import path from 'path';
 import { Configuration, EnvironmentPlugin } from 'webpack';
-import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const publicPath = '/';
 const fromRoot = path.resolve.bind(null, __dirname);
 const isProduction = process.env.NODE_ENV === 'production';
-const config: Configuration & { devServer?: WebpackDevServerConfiguration } = {
+const config: Configuration = {
   mode: isProduction ? 'production' : 'development',
   entry: {
     app: ['./src/index.ts'],
