a1026bb9d0f6820efa4f55bdc2c5647a66febf8f
Kav Singh <flakisms@gmail.com>
2020-04-09 13:19:09 +0200

    remove redundant jest transform

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   verbose: true,
   testRegex: '^.+\\.test\\.[jt]s?$',
-  transform: {
-    '^.+\\.[jt]s?$': '<rootDir>/node_modules/babel-jest',
-  },
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   moduleNameMapper: {
