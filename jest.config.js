03312524e91ad5cb239066e2190848d9aac3825d
Kav Singh <flakisms@gmail.com>
2020-04-24 00:08:33 +0200

    trim jest config, git ignore coverage artefacts

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   verbose: true,
-  testRegex: '^.+\\.test\\.[jt]s?$',
-  moduleDirectories: ['node_modules', 'src'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  moduleNameMapper: {
-    '^~/(.*)': '<rootDir>/src/$1',
-  },
-  coverageReporters: ['lcov'],
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: { '^~/(.*)': '<rootDir>/src/$1' },
 };
