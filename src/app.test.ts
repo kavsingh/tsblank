161489ecd79e8a37cf06183213fb9ccb962d9bc6
Kav Singh <flakisms@gmail.com>
2021-09-12 20:22:23 +0200

    simplify test setup / cleanup

--- src/app.test.ts
+++ src/app.test.ts
@@ -4,15 +4,12 @@ import app from './app';
 
 describe('app test', () => {
   beforeEach(() => {
-    Array.from(document.body.children).forEach((el) => {
-      document.body.removeChild(el);
-    });
+    document.body.innerHTML = '';
   });
 
   it('initializes app', () => {
-    const root = document.createElement('div');
+    const root = document.body.appendChild(document.createElement('div'));
 
-    document.body.appendChild(root);
     app(root);
 
     expect(screen.getByText('hello')).toBeInTheDocument();
