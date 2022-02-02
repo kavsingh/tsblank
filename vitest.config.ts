379f4a4f842beb93234fb8b7339a9f0a8b9a1409
Kav Singh <flakisms@gmail.com>
2022-02-02 10:54:19 +0800

    fix test config for path aliasing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 
+import viteConfig from './vite.config';
+
 export default defineConfig({
+  ...viteConfig,
   test: {
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}'],
     environment: 'jsdom',
