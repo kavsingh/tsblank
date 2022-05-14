939d3baa52d1b46f080cd686b5dd6f6bf63ad60b
Kav Singh <flakisms@gmail.com>
2022-05-14 21:58:45 +0200

    add checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import path from "path";
 
 import { defineConfig } from "vite";
 import legacyPlugin from "@vitejs/plugin-legacy";
+import checkerPlugin from "vite-plugin-checker";
+
+const checker = checkerPlugin({
+  overlay: { initialIsOpen: false },
+  typescript: true,
+  eslint: {
+    lintCommand: 'eslint "./src/**/*.ts"',
+    dev: { logLevel: ["error"] },
+  },
+});
 
 export default defineConfig({
   build: { sourcemap: true },
-  plugins: [legacyPlugin()],
+  plugins: [checker, legacyPlugin()],
   resolve: { alias: { "~": path.resolve(__dirname, "./src") } },
   test: {
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}"],
