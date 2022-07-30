ea62d01d4e7bb6a46db25c82e74ccd9058ef6ac4
Kav Singh <flakisms@gmail.com>
2022-07-31 00:26:02 +0200

    update deps, config

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,22 @@ import path from "path";
 import { defineConfig } from "vite";
 import checkerPlugin from "vite-plugin-checker";
 
-const checker = checkerPlugin({
-	overlay: { initialIsOpen: false },
-	typescript: true,
-	eslint: {
-		lintCommand: 'eslint "./src/**/*.ts"',
-		dev: { logLevel: ["error"] },
-	},
-});
-
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	build: { sourcemap: true },
-	plugins: [checker],
+	plugins: [...(mode === "test" ? [] : [checker()])],
 	resolve: { alias: { "~": path.resolve(__dirname, "./src") } },
 	test: {
 		include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}"],
 		environment: "jsdom",
 	},
-});
+}));
+
+const checker = () =>
+	checkerPlugin({
+		overlay: { initialIsOpen: false },
+		typescript: true,
+		eslint: {
+			lintCommand: 'eslint "./src/**/*.ts"',
+			dev: { logLevel: ["error"] },
+		},
+	});
