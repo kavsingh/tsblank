d0cebdbd27e2030c1132c2e23b729453af025e42
Kav Singh <flakisms@gmail.com>
2022-08-14 16:39:17 +0200

    update deps, fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,7 +70,7 @@ module.exports = {
 				"plugin:@typescript-eslint/recommended",
 				"plugin:@typescript-eslint/recommended-requiring-type-checking",
 			],
-			plugins: ["deprecated"],
+			plugins: ["deprecation"],
 			rules: {
 				"camelcase": "off",
 				"no-shadow": "off",
