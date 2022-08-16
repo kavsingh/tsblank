e6d5f29d34caa9eb885dade6465a15d5a7d703c4
Kav Singh <flakisms@gmail.com>
2022-08-16 19:03:31 +0200

    add strict typescript eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,6 +69,7 @@ module.exports = {
 			extends: [
 				"plugin:@typescript-eslint/recommended",
 				"plugin:@typescript-eslint/recommended-requiring-type-checking",
+				"plugin:@typescript-eslint/strict",
 			],
 			plugins: ["deprecation"],
 			rules: {
