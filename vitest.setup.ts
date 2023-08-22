b0f71a9295e610db8aad04ca80f9ed80abe7f0f4
Kav Singh <flakisms@gmail.com>
2023-08-22 23:38:51 +0200

    replace jest dom with vitest-dom

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,17 +1,4 @@
-import matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="vitest-dom/extend-expect" />
 
-import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-
-expect.extend(matchers);
-
-declare global {
-	// eslint-disable-next-line @typescript-eslint/no-namespace
-	namespace Vi {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-empty-interface
-		interface JestAssertion<T = any> extends ExtendedJestMatchers<T> {}
-	}
-}
-
-type ExtendedJestMatchers<T> = jest.Matchers<void, T> &
-	TestingLibraryMatchers<T, void>;
+import "vitest-dom/extend-expect";
