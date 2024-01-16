f90194f6e82ac89ca1756db48fc66b760f3f2fd7
Kav Singh <flakisms@gmail.com>
2024-01-16 21:37:04 +0100

    update deps

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -5,7 +5,7 @@ export default function Button({ label, onClick, className }: ButtonProps) {
 
 	frag.innerHTML = `
 		<button class="${twMerge(
-			"flex h-6 w-6 items-center justify-center rounded-full border border-violet-300 bg-transparent leading-none text-current transition-colors hover:border-violet-500 dark:border-violet-900 dark:hover:border-violet-600",
+			"flex size-6 items-center justify-center rounded-full border border-violet-300 bg-transparent leading-none text-current transition-colors hover:border-violet-500 dark:border-violet-900 dark:hover:border-violet-600",
 			className,
 		)}">${label}</button>
 	`;
