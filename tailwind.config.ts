68f070d918f7009228e57bc1e2754cd328e69905
Kav Singh <flakisms@gmail.com>
2024-03-08 21:29:10 +0100

    preact

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import containerQueriesPlugin from "@tailwindcss/container-queries";
 import type { Config } from "tailwindcss";
 
 export default {
-	content: ["./index.html", "./src/**/*.{ts,js}"],
+	content: ["./index.html", "./src/**/*.{tsx,ts,jsx,js}"],
 	plugins: [
 		// @ts-expect-error exactOptionalProperties conflict
 		containerQueriesPlugin,
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import path from "node:path";
 import { URL, fileURLToPath } from "node:url";
 
+import preactPlugin from "@preact/preset-vite";
 import { defineConfig } from "vite";
 import checkerPlugin from "vite-plugin-checker";
 import tsconfigPathsPlugin from "vite-tsconfig-paths";
@@ -17,10 +18,11 @@ export default defineConfig(({ mode }) => ({
 		tsconfigPathsPlugin({
 			projects: [path.resolve(__dirname, "src/tsconfig.json")],
 		}),
+		preactPlugin(),
 		checker(mode),
 	] as PluginOption[],
 	test: {
-		include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts}"],
+		include: ["src/**/*.{test,spec}.{js,jsx,mjs,cjs,ts,tsx,mts,cts}"],
 		environment: "jsdom",
 		setupFiles: ["./vitest.setup.ts"],
 		clearMocks: true,
