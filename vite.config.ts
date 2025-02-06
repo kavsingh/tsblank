d4f6df66fe9557674ce06a7ea58489e72039b6fb
Kav Singh <flakisms@gmail.com>
2025-02-06 19:51:47 +0100

    chore: use new bun lock, cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
-/// <reference types="vitest" />
-
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
 import { defineConfig } from "vite";
 import { checker as checkerPlugin } from "vite-plugin-checker";
 import tsconfigPathsPlugin from "vite-tsconfig-paths";
 
-import type { PluginOption } from "vite";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig(({ mode }) => ({
 	build: { sourcemap: true },
-	plugins: [
-		tsconfigPathsPlugin({
-			projects: [path.resolve(__dirname, "src/tsconfig.json")],
-		}),
-		checker(mode),
-	] as PluginOption[],
+	plugins: [tsconfigPathsPlugin(), checker(mode)],
 }));
 
 function checker(mode: string) {
