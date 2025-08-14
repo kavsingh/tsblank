6f8804236a03a510b5297ce8491189c7563bfa31
Kav Singh <flakisms@gmail.com>
2025-08-14 15:46:07 +0200

    basic app

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { NuqsAdapter } from "nuqs/adapters/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
@@ -12,6 +13,8 @@ const root = createRoot(rootEl);
 
 root.render(
 	<StrictMode>
-		<App />
+		<NuqsAdapter>
+			<App />
+		</NuqsAdapter>
 	</StrictMode>,
 );
