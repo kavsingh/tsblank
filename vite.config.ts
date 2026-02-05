fa2bb20f5630e10befa045c8c2a76cfe3e18de2c
Kav Singh <flakisms@gmail.com>
2026-02-05 16:24:49 +0100

    solid-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { checker } from "vite-plugin-checker";
+import solid from "vite-plugin-solid";
 
 export default defineConfig(({ mode }) => {
 	return {
 		resolve: { tsconfigPaths: true },
+		oxc: { jsx: { importSource: "solid-js" } },
 		plugins: [
 			tailwindcss(),
+			solid(),
 			mode === "development"
 				? checker({ oxlint: true, overlay: { initialIsOpen: false } })
 				: undefined,
