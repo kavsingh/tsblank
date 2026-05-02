74d07171b3dfd2e3e1bddc4a0147563f4b61d8e6
Kav Singh <flakisms@gmail.com>
2026-05-02 19:08:40 +0200

    refactor: some vite cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { checker } from "vite-plugin-checker";
 
-function createChecker(mode: string) {
-	if (mode !== "development") return undefined;
-
-	return checker({
-		overlay: { initialIsOpen: false },
-		oxlint: { lintCommand: "oxlint --type-aware --type-check" },
-	});
-}
-
 export default defineConfig(({ mode }) => {
 	return {
 		resolve: { tsconfigPaths: true },
-		plugins: [tailwindcss(), createChecker(mode)],
+		plugins: [
+			tailwindcss(),
+			mode === "development"
+				? checker({ oxlint: true, overlay: { initialIsOpen: false } })
+				: undefined,
+		],
 	};
 });
